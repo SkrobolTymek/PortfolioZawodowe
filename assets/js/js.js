765b496b33f8ca306c53a2fd468c10832cda1fcc
a <a>
2025-04-15 07:25:34 +0200

    tak

---
+++ assets/js/js.js
@@ -0,0 +1,103 @@
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+alert("NIE JESTEM PROGRAMISTA FRONTENDOWYM, PROSZE WZIAC TO POD UWAGE!!");
+
+/*
+
+fn
+f
+wfw
+fw
+f
+wf
+w
+fw
+f
+wf
+w
+fwf
+wf
+wf
+wfw
+f
+wfwfwfw
+f
+f
+w
+f
+w
+fw
+f
+wfwf
+wf
+wf
+wf
+wf
+w
+fw
+fw
+fw
+f
+wfw
+fwf
+w
+fw
+f
+
+wfw
+fwf
+wfwf
+w
+fw
+f
+wfwf
+w
+fw
+fw
+fw
+fw
+f
+w
+fw
+fw
+f
+wf
+wf
+w
+fw
+f
+wfw
+f
+wf
+wffwf
+w
+fw
+fw
+fw
+fw
+f
+ww
+w
+w
+fw
+
+fw
+f
+
+*/
